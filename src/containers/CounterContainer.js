b010aadab32a09b106211fa8520a6d41cadac018
bombi <bombic3@naver.com>
2022-07-27 13:22:37 +0900

    REFACTOR : useCallback 사용하여 성능 최적화하기

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
@@ -5,12 +6,14 @@ import { increase, decrease } from '../modules/counter';
 const CounterContainer = () => {
   const number = useSelector(state => state.counter.number);
   const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
   return (
     <div>
       <Counter
         number={number}
-        onIncrease={() => dispatch(increase())}
-        onDecrease={() => dispatch(decrease())}
+        onIncrease={onIncrease}
+        onDecrease={onDecrease}
       />
     </div>
   );
